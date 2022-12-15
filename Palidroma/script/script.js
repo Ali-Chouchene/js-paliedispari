c6ae7d50338a283976af8a0eb6771661482a5b97
Ali-Chouchene <choucheneali9801@gmail.com>
2022-12-15 17:31:06 +0100

    ended

--- Palidroma/script/script.js
+++ Palidroma/script/script.js
@@ -6,4 +6,39 @@ Creare una funzione per capire se la parola inserita è palindroma
 // * 1. Scriviamo sempre in italiano i passaggi che vogliamo fare.
 // * 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 
+function invertWord(word) {
+    let reversed = "";
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversed += word[i];
+    }
+    return reversed;
+}
+
+
+
+
 // ! start
+
+const form = document.getElementById("form");
+const result = document.getElementById("paste");
+
+
+let message = "";
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const inputName = document.getElementById("input").value;
+    const finalName = invertWord(inputName);
+
+
+    if (finalName === inputName) {
+        message = finalName;
+
+    } else {
+        message = `la parola ${inputName}  non è Polidroma.`;
+    }
+    result.classList.remove("d-none")
+    result.innerText = message;
+    return
+})
+
